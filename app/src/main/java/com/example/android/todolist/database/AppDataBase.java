5d3696f7602bc7707de7c7ce569575a3939109da
luispuchades <luispuchades@live.com>
2018-06-20 06:48:37 +0200

    Creating the Database

---
+++ app/src/main/java/com/example/android/todolist/database/AppDataBase.java
@@ -0,0 +1,29 @@
+package com.example.android.todolist.database;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+import android.util.Log;
+
+@Database(entities = {TaskEntry.class}, version = 1, exportSchema = false)
+public abstract class AppDataBase extends RoomDatabase {
+
+    private static final String LOG_TAG = AppDataBase.class.getSimpleName();
+    private static final Object LOCK = new Object();
+    private static final String DATABASE_NAME = "todolist";
+    private static AppDataBase sInstance;
+
+    public static AppDataBase getInstance(Context context) {
+        if (sInstance == null) {
+            synchronized (LOCK) {
+                Log.d(LOG_TAG, "Creating new database instance");
+                sInstance = Room.databaseBuilder(context.getApplicationContext(),
+                        AppDataBase.class, AppDataBase.DATABASE_NAME)
+                        .build();
+            }
+        }
+        Log.d(LOG_TAG, "Getting the database instance");
+        return sInstance;
+    }
+}
